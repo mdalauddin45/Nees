402747193aea1562ccb3f29e95fe9573e7bad732
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-01-15 23:15:08 +0600

    profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,20 +1,24 @@
 from django.contrib.auth.models import User
 from django import forms
-from .models import UserAccount
+from .models import UserLibraryAccount
 from django.contrib.auth.forms import UserCreationForm
+from accounts.models import UserLibraryAccount
 
 class UserRegistrationForm(UserCreationForm):
+    birth_date = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
     class Meta:
         model = User 
-        fields = [ 'username','first_name', 'last_name','email', 'password1', 'password2']
+        fields = [ 'username','password1', 'password2', 'first_name', 'last_name', 'birth_date','email']
     
     def save(self, commit=True):
         our_user = super().save(commit=False)
         if commit == True:
             our_user.save()
-            UserAccount.objects.create(
+            birth_date = self.cleaned_data.get('birth_date')
+            UserLibraryAccount.objects.create(
                 user = our_user,
-                account_no = 1212 + our_user.id
+                birth_date =birth_date,
+                account_no = 1000 + our_user.id
             )
         return our_user     
     
@@ -32,39 +36,39 @@ class UserRegistrationForm(UserCreationForm):
                 ) 
             })
             
-# class UserUpdateForm(forms.ModelForm):
-#     birth_date = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
-#     class Meta:
-#         model = User
-#         fields = ['first_name', 'last_name', 'email']
+class UserUpdateForm(forms.ModelForm):
+    birth_date = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
 
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         for field in self.fields:
-#             self.fields[field].widget.attrs.update({
-#                 'class': (
-#                     'appearance-none block w-full bg-gray-200 '
-#                     'text-gray-700 border border-gray-200 rounded '
-#                     'py-3 px-4 leading-tight focus:outline-none '
-#                     'focus:bg-white focus:border-gray-500'
-#                 )
-#             })
-#         if self.instance:
-#             try:
-#                 user_account = self.instance.account
-#             except UserAccount.DoesNotExist:
-#                 user_account = None
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': (
+                    'appearance-none block w-full bg-gray-200 '
+                    'text-gray-700 border border-gray-200 rounded '
+                    'py-3 px-4 leading-tight focus:outline-none '
+                    'focus:bg-white focus:border-gray-500'
+                )
+            })
+        if self.instance:
+            try:
+                user_account = self.instance.account
+            except UserLibraryAccount.DoesNotExist:
+                user_account = None
 
-#             if user_account:
-#                 self.fields['birth_date'].initial = user_account.birth_date
+            if user_account:
+                self.fields['birth_date'].initial = user_account.birth_date
 
-#     def save(self, commit=True):
-#         user = super().save(commit=False)
-#         if commit:
-#             user.save()
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if commit:
+            user.save()
 
-#             user_account, created = UserAccount.objects.get_or_create(user=user)
-#             user_account.birth_date = self.cleaned_data['birth_date']
-#             user_account.save()
+            user_account, created = UserLibraryAccount.objects.get_or_create(user=user)
+            user_account.birth_date = self.cleaned_data['birth_date']
+            user_account.save()
 
-#         return user
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm,UserUpdateForm
 from django.urls import reverse_lazy
 from django.views.generic import FormView
 from django.contrib.auth import login, logout
@@ -7,94 +7,21 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import ListView
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .serializers import RegistrationSerializer,UserLoginSerializer
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.contrib.auth.tokens import default_token_generator
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.utils.encoding import force_bytes
-from django.template.loader import render_to_string
-from django.core.mail import EmailMultiAlternatives
-from django.contrib.auth.models import User
-from rest_framework import viewsets
-from django.contrib.auth import authenticate, login, logout
-from rest_framework.authtoken.models import Token
-from .models import UserAccount
-from django.contrib.auth import get_user_model
-from .serializers import UserSerializer
-# Create your views here.
-class UserViewset(viewsets.ModelViewSet):
-    queryset = UserAccount.objects.all()
-    serializer_class = UserSerializer
-    
-# class UserRegistrationView(FormView):
-#     template_name = 'accounts/user_registration.html'
-#     form_class = UserRegistrationForm
-#     serializer_class = RegistrationSerializer
-    
-#     def form_valid(self, form):
-#         user = form.save()
-#         messages.success(self.request, "After registration, Login successfully")
-#         return redirect('login')
-    
-#     def post(self, request):
-#         serializer = self.serializer_class(data=request.data)
-#         if serializer.is_valid():
-#             user = serializer.save()
-#             print("Registration", user)
-#             token = default_token_generator.make_token(user)
-#             print("token", token)
-#             uid = urlsafe_base64_encode(force_bytes(user.pk))
-#             print("uid", uid)
-#             confirm_link = f'http://127.0.0.1:8000/accounts/active/{uid}/{token}'
-#             print("confirm_link", confirm_link)
-#             email_subject = "Confirm Your Email"
-#             email_body = render_to_string('accounts/confirm_email.html',{'confirm_link': confirm_link})
-#             send_email = EmailMultiAlternatives(email_subject, '',to=[user.email])
-#             send_email.attach_alternative(email_body, "text/html")
-#             send_email.send()
-#             return Response("Check your email address for confirmation")
-#         return Response(serializer.errors)
 
+# Create your views here.
 class UserRegistrationView(FormView):
     template_name = 'accounts/user_registration.html'
     form_class = UserRegistrationForm
-    success_url = reverse_lazy('login') 
-
-    def form_valid(self, form):
+    success_url = reverse_lazy('profile')
+    
+    def form_valid(self,form):
+        print(form.cleaned_data)
         user = form.save()
-
-        token = default_token_generator.make_token(user)
-        uid = urlsafe_base64_encode(force_bytes(user.pk))
-        confirm_link = f'http://127.0.0.1:8000/accounts/activate/{uid}/{token}'
-        email_subject = "Confirm Your Email"
-        email_body = render_to_string('accounts/confirm_email.html', {'confirm_link': confirm_link})
-        send_email = EmailMultiAlternatives(email_subject, '', to=[user.email])
-        send_email.attach_alternative(email_body, "text/html")
-        send_email.send()
-
-        messages.success(self.request, "Check your email for confirmation.")
+        login(self.request, user)
+        print(user)
+        messages.success(self.request, "After registration , Login successfully")
         return super().form_valid(form)
-
-    def form_invalid(self, form):
-        return super().form_invalid(form)
-
-def activate(request, uid64, token):
-    print("uid64:", uid64)
-    print("token:", token)
-    try:
-        uid = urlsafe_base64_decode(uid64).decode()
-        User = get_user_model()
-        user = User._default_manager.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-        user = None
-    if user is not None and default_token_generator.check_token(user, token):
-        user.is_active = True
-        user.save()
-        messages.success(request, 'Your account is now activated. You can log in.')
-        return redirect('login')
-    messages.error(request, 'Invalid activation link. Please try again or contact support.')
-    return redirect('register') 
+    
 
 class UserLoginView(LoginView):
     template_name = 'accounts/user_login.html'
@@ -111,64 +38,23 @@ class UserLogoutView(LogoutView):
 
 class ProfileView(LoginRequiredMixin, ListView):
     template_name = 'accounts/profile.html'
+    balance = 0
 
-class UserRegistrationApiView(APIView):
-    serializer_class = RegistrationSerializer
+    def get(self, request):
+        user_purchases = Bookpurchase.objects.filter(user=request.user)
+        account_balance = request.user.account.balance
+
+        form = UserUpdateForm(instance=request.user)
+        return render(request, self.template_name, {
+            'user_purchases': user_purchases,
+            'account_balance': account_balance,
+            'form': form,
+        })
+
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            print("Registration", user)
-            token = default_token_generator.make_token(user)
-            print("token", token)
-            uid = urlsafe_base64_encode(force_bytes(user.pk))
-            print("uid", uid)
-            confirm_link = f'http://127.0.0.1:8000/accounts/active/{uid}/{token}'
-            print("confirm_link", confirm_link)
-            email_subject = "Confirm Your Email"
-            email_body = render_to_string('accounts/confirm_email.html',{'confirm_link': confirm_link})
-            send_email = EmailMultiAlternatives(email_subject, '',to=[user.email])
-            send_email.attach_alternative(email_body, "text/html")
-            send_email.send()
-            return Response("Check your email address for confirmation")
-        return Response(serializer.errors)
-
-# def activate(request, uid64, token):
-#     try:
-#         uid = urlsafe_base64_decode(uid64).decode()
-#         user = User._default_manager.get(pk=uid)
-#     except(User.DoesNotExist):
-#         user = None
-#     if user is not None and default_token_generator.check_token(user, token):
-#         user.is_active = True
-#         user.save()
-#         return redirect('login')
-#     else:
-#       return redirect('register')  
-        # return HttpResponse('Activation link is invalid or expired.')  
-
-
-class UserLoginApiView(APIView):
-    def post(self,request):
-        serializer = UserLoginSerializer(data=self.request.data)
-        if serializer.is_valid():
-            username = serializer.validated_data['username']
-            password = serializer.validated_data['password']
-            
-            user = authenticate(username=username, password=password)
-            
-            if user:
-                token, created = Token.objects.get_or_create(user=user)
-                print(token)
-                print(created)
-                login(request,user)
-                return Response({'token': token.key, 'user_id': user.id})
-            else:
-                return Response({'error': 'Invalid username or password'})
-        return Response(serializer.errors)
-                
-class UserLogoutApiView(APIView):
-    def get(self,request):
-        request.user.auth_token.delete()
-        logout(request)
-        return redirect('login')
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully!')
+            return redirect('profile')
+        return render(request, self.template_name, {'form': form})

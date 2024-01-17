77511f2d28125f90e37fc51f8eeadc118b4c1599
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-01-17 23:37:48 +0600

    email urls done

--- accounts/views.py
+++ accounts/views.py
@@ -30,7 +30,7 @@ class UserRegistrationView(FormView):
         
         token = default_token_generator.make_token(user)
         uid = urlsafe_base64_encode(force_bytes(user.pk))
-        confirm_link = f'http://127.0.0.1:8000/accounts/active/{uid}/{token}'
+        confirm_link = f'https://neeshorgo.onrender.com/accounts/active/{uid}/{token}'
         email_subject = "Confirm Your Email"
         email_body = render_to_string('accounts/confirm_email.html', {'confirm_link': confirm_link})
         send_email = EmailMultiAlternatives(email_subject, '', to=[user.email])
